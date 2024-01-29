556baa9cd25ebcc51d657e0297bff9eb3e419e0a
Thiago Hardt <thiagohrh@outlook.com>
2024-01-29 03:34:44 -0300

    Death condition event created

--- Assets/Project/Source/Enemies/ZombieHealthComponent.cs
+++ Assets/Project/Source/Enemies/ZombieHealthComponent.cs
@@ -1,17 +1,27 @@
+using System;
 using UnityEngine;
 
 public class ZombieHealthComponent : MonoBehaviour
 {
     private const int  MaxTotalHealth = 3;
     private int _currentHealth;
+    public event Action OnZombieDeath;
 
     protected void OnEnable()
     {
         _currentHealth = MaxTotalHealth;
     }
 
-    public void ApplyDamageValue(float value)
+    public void ApplyDamageValue(int value)
     {
-        
+        _currentHealth -= value;
+    }
+
+    private void CheckDeathCondition()
+    {
+        if (_currentHealth <= 0)
+        {
+            OnZombieDeath?.Invoke();
+        }
     }
 }
